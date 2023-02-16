520afcaffb551779e6f526db89a948cb397eadc9
markfialko <markfialkofialko@gmail.com>
2023-02-16 12:10:49 +0500

    Фиалко Марк exercises01

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -11,7 +11,8 @@ class Vector(val x: Double, val y: Double) {
 
   def euclideanLength: Double = Math.sqrt(x * x + y * y)
 
-  def normalized: Vector = if (x == 0 && y == 0) new Vector(0,0) else  new Vector(x / euclideanLength, y / euclideanLength)
+  def normalized: Vector =
+    if (x == 0 && y == 0) new Vector(0, 0) else new Vector(x / euclideanLength, y / euclideanLength)
   override def equals(other: Any): Boolean = true
 
   // Vector(x, y)
@@ -21,7 +22,7 @@ class Vector(val x: Double, val y: Double) {
 object Vector {
   def fromAngle(angle: Double, length: Double): Vector = new Vector(length * Math.cos(angle), length * Math.sin(angle))
 
-  def sum(list: List[Vector]): Vector = list.foldRight(new Vector(0,0))((A,B) => new Vector(A.x + B.x, A.y + B.y))
+  def sum(list: List[Vector]): Vector = list.foldRight(new Vector(0, 0))((A, B) => new Vector(A.x + B.x, A.y + B.y))
 
   def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x, arg.y)
 }
