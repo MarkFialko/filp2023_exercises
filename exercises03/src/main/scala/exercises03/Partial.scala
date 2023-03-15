decb6ba1aad0bcd274f81b6d6f408d9484ba174c
markfialko <markfialkofialko@gmail.com>
2023-03-15 20:15:55 +0500

    Фиалко Марк exercises03

--- exercises03/src/main/scala/exercises03/Partial.scala
+++ exercises03/src/main/scala/exercises03/Partial.scala
@@ -1,5 +1,7 @@
 package exercises03
 
 object Partial {
-  def combo[I, T](funcs: List[PartialFunction[I, T]]): I => Option[T] = ???
+  def combo[I, T](funcs: List[PartialFunction[I, T]]): I => Option[T] = { input: I =>
+    funcs.foldLeft(Option.empty[T])((acc, f) => if (acc.isDefined) acc else f.lift(input) orElse acc)
+  }
 }
