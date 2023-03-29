6962f8429689ce8fdf7c3e507438232d2e8e5f14
markfialko <markfialkofialko@gmail.com>
2023-03-30 04:10:11 +0500

    Фиалко  Марк exercises05

--- exercises05/src/main/scala/exercises05/DiffList.scala
+++ exercises05/src/main/scala/exercises05/DiffList.scala
@@ -1,28 +1,28 @@
 package exercises05
 
 /**
-  * Необходимо реализовать prepend, append, withFilter и toList
-  * Метод prepend принимает на вход список s, добавляемый в начало
-  * Вернуть надо новый объект, сконструировав его от новой функции
-  * Эта функция должна возвращать список s , добавленный в начало к списку, возвращаемому из calculate
-  * Метод append принимает на вход список s, добавляемый в конец.
-  * Вернуть надо новый объект, сконструировав его от новой функции.
-  * Эта функция должна возвращать результат применения функции calculate к конкатенации списка s и аргумента этой функции.
-  * withFilter должна проверить предикат по всем элементам списка
-  * Метод toList применяет все накопленные операции и отдаёт итоговый список.
-  */
+ * Необходимо реализовать prepend, append, withFilter и toList
+ * Метод prepend принимает на вход список s, добавляемый в начало
+ * Вернуть надо новый объект, сконструировав его от новой функции
+ * Эта функция должна возвращать список s , добавленный в начало к списку, возвращаемому из calculate
+ * Метод append принимает на вход список s, добавляемый в конец.
+ * Вернуть надо новый объект, сконструировав его от новой функции.
+ * Эта функция должна возвращать результат применения функции calculate к конкатенации списка s и аргумента этой функции.
+ * withFilter должна проверить предикат по всем элементам списка
+ * Метод toList применяет все накопленные операции и отдаёт итоговый список.
+ */
 final class DiffList[A](calculate: List[A] => List[A]) {
-  def prepend(s: List[A]): DiffList[A] = ???
+  def prepend(s: List[A]): DiffList[A] = new DiffList[A](elem => s ::: calculate(elem))
 
-  def append(s: List[A]): DiffList[A] = ???
+  def append(s: List[A]): DiffList[A] = new DiffList[A](elem => calculate(elem) ::: s)
 
-  def withFilter(f: A => Boolean): DiffList[A] = ???
+  def withFilter(f: A => Boolean): DiffList[A] = new DiffList[A](elem => calculate(elem).filter(f))
 
-  def toList: List[A] = ???
+  def toList: List[A] = calculate(Nil)
 }
 
 object DiffList extends App {
-  def apply[A]: DiffList[A] = ???
+  def apply[A]: DiffList[A] = new DiffList[A](identity)
 
-  def singletonList[A](el: A): DiffList[A] = ???
+  def singletonList[A](el: A): DiffList[A] = new DiffList[A](_ => List(el))
 }
--- exercises05/src/main/scala/exercises05/parser/Examples.scala
+++ exercises05/src/main/scala/exercises05/parser/Examples.scala
@@ -6,28 +6,54 @@ import Error._
 object Examples {
 
   /**
-    * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть None
-    * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
-    * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть None
-    * если rawUser.id не парсится в Long то функция должна вернуть None
-    * если rawUser.banned, то вернуть None
-    * используйте for-comprehension
-    */
-  def transformToOption(rawUser: RawUser): Option[User] = ???
+   * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть None
+   * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
+   * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть None
+   * если rawUser.id не парсится в Long то функция должна вернуть None
+   * если rawUser.banned, то вернуть None
+   * используйте for-comprehension
+   */
+  def transformToOption(rawUser: RawUser): Option[User] =
+    for (firstName  <- rawUser.firstName if rawUser.firstName.isDefined;
+         secondName <- rawUser.secondName if rawUser.secondName.isDefined;
+         id         <- rawUser.id.toLongOption;
+         passport   <- checkPassport(rawUser.passport)
+         if !rawUser.banned) yield User(id, UserName(firstName, secondName, rawUser.thirdName), passport)
+
+  private def checkPassport(passport: Option[String]): Option[Option[Passport]] = passport match {
+    case Some(value) =>
+      val res = value.split(" ")
+      res.length match {
+        case 2 =>
+          (res(0).toLongOption, res(1).toLongOption) match {
+            case (Some(value1), Some(value2)) if value1 < 10000 && value2 < 1000000 =>
+              Some(Some(Passport(value1, value2)))
+            case _ => None
+          }
+        case _ => None
+      }
+    case None => Some(None)
+  }
 
   /**
-    * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть Left(InvalidName)
-    * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
-    * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть Left(InvalidPassport)
-    * если rawUser.id не парсится в Long то функция должна вернуть Left(InvalidId)
-    * если rawUser.banned, то вернуть Left(Banned)
-    * у ошибок есть приоритет:
-    * 1. Banned
-    * 2. InvalidId
-    * 3. InvalidName
-    * 4. InvalidPassport
-    * используйте for-comprehension
-    * но для того, чтобы for-comprehension заработал надо реализовать map и flatMap в Either
-    */
-  def transformToEither(rawUser: RawUser): Either[Error, User] = ???
+   * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть Left(InvalidName)
+   * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
+   * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть Left(InvalidPassport)
+   * если rawUser.id не парсится в Long то функция должна вернуть Left(InvalidId)
+   * если rawUser.banned, то вернуть Left(Banned)
+   * у ошибок есть приоритет:
+   * 1. Banned
+   * 2. InvalidId
+   * 3. InvalidName
+   * 4. InvalidPassport
+   * используйте for-comprehension
+   * но для того, чтобы for-comprehension заработал надо реализовать map и flatMap в Either
+   */
+  def transformToEither(rawUser: RawUser): Either[Error, User] =
+    for (_          <- if (rawUser.banned) Left(Banned) else Right();
+         id         <- Either.fromOption(rawUser.id.toLongOption)(InvalidId);
+         firstName  <- Either.fromOption(rawUser.firstName)(InvalidName);
+         secondName <- Either.fromOption(rawUser.secondName)(InvalidName);
+         passport   <- Either.fromOption(checkPassport(rawUser.passport))(InvalidPassport))
+    yield User(id, UserName(firstName, secondName, rawUser.thirdName), passport)
 }
