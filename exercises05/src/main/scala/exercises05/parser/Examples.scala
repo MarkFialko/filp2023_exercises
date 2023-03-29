0feb1e09692851ac62381b57d2ee5db90ca8caca
markfialko <markfialkofialko@gmail.com>
2023-03-30 04:13:29 +0500

    Фиалко  Марк exercises05

--- exercises05/src/main/scala/exercises05/parser/Examples.scala
+++ exercises05/src/main/scala/exercises05/parser/Examples.scala
@@ -6,13 +6,13 @@ import Error._
 object Examples {
 
   /**
-   * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть None
-   * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
-   * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть None
-   * если rawUser.id не парсится в Long то функция должна вернуть None
-   * если rawUser.banned, то вернуть None
-   * используйте for-comprehension
-   */
+    * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть None
+    * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
+    * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть None
+    * если rawUser.id не парсится в Long то функция должна вернуть None
+    * если rawUser.banned, то вернуть None
+    * используйте for-comprehension
+    */
   def transformToOption(rawUser: RawUser): Option[User] =
     for (firstName  <- rawUser.firstName if rawUser.firstName.isDefined;
          secondName <- rawUser.secondName if rawUser.secondName.isDefined;
@@ -36,24 +36,24 @@ object Examples {
   }
 
   /**
-   * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть Left(InvalidName)
-   * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
-   * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть Left(InvalidPassport)
-   * если rawUser.id не парсится в Long то функция должна вернуть Left(InvalidId)
-   * если rawUser.banned, то вернуть Left(Banned)
-   * у ошибок есть приоритет:
-   * 1. Banned
-   * 2. InvalidId
-   * 3. InvalidName
-   * 4. InvalidPassport
-   * используйте for-comprehension
-   * но для того, чтобы for-comprehension заработал надо реализовать map и flatMap в Either
-   */
+    * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть Left(InvalidName)
+    * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
+    * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть Left(InvalidPassport)
+    * если rawUser.id не парсится в Long то функция должна вернуть Left(InvalidId)
+    * если rawUser.banned, то вернуть Left(Banned)
+    * у ошибок есть приоритет:
+    * 1. Banned
+    * 2. InvalidId
+    * 3. InvalidName
+    * 4. InvalidPassport
+    * используйте for-comprehension
+    * но для того, чтобы for-comprehension заработал надо реализовать map и flatMap в Either
+    */
   def transformToEither(rawUser: RawUser): Either[Error, User] =
     for (_          <- if (rawUser.banned) Left(Banned) else Right();
          id         <- Either.fromOption(rawUser.id.toLongOption)(InvalidId);
          firstName  <- Either.fromOption(rawUser.firstName)(InvalidName);
          secondName <- Either.fromOption(rawUser.secondName)(InvalidName);
          passport   <- Either.fromOption(checkPassport(rawUser.passport))(InvalidPassport))
-    yield User(id, UserName(firstName, secondName, rawUser.thirdName), passport)
+      yield User(id, UserName(firstName, secondName, rawUser.thirdName), passport)
 }
